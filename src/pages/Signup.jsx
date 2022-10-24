b4e25dba8dc135d251a9e420496611208083f5ea
ujeneza divine <ujenezadivine@gmail.com>
2022-10-24 18:44:56 +0200

    addition lines

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import  {useState} from "react";
 import Validation from './validationSignup';
-// import Logo from '../image/logo.png';
-import {FcGoogle} from 'react-icons/fc'
-import {SiFacebook} from 'react-icons/si'
-import {GrLinkedin} from 'react-icons/gr'
+// import {FcGoogle} from 'react-icons/fc'
+// import {SiFacebook} from 'react-icons/si'
+// import {GrLinkedin} from 'react-icons/gr'
  function SignUp(){
      const[values, setValues] = useState({
          email:"",
@@ -28,21 +27,20 @@ import {GrLinkedin} from 'react-icons/gr'
      }
     
     return(
-        // <div className="bg-gray-100 h-screen grid items-center ">
       <div className=" grid gri-cols-1 sm:grid-cols-2 h-screen w-[100%] mx-auto overflow-hidden ">
-          <div className="hidden sm:block bg-blue-800 h-screen">
+          <div className="hidden sm:block bg-blue-900 h-screen">
               hy
           </div>
-          <div className="w-[100%] mx-auto bg-blue-800 ">
+          <div className="w-[100%] mx-auto ">
               <form className="max-w-[100%] w-full max-auto bg-white p-20  h-screen gb-red-300">
                   <h2 className="text-4xl font-bold text-center py-6">Create new account</h2>
                   <p className=" text-center">No more wasting of time and product</p>
                   <div className="flex flex-col py-3">
-                      <input className="border p-2 rounded-lg  w-[100%]" type="text" placeholder="Enter your Email/phone number " name="email" value={values.email} onChange={handleChange }/>
+                      <input className="border p-2 rounded-lg  w-[100%] outline-none" type="text" placeholder="Enter your Email/phone number " name="email" value={values.email} onChange={handleChange }/>
                       {errors.email && <p className="error">{errors.email}</p>}
                   </div>
                   <div className="flex flex-col py-3">
-                      <input className="border p-2 rounded-lg" type="password" placeholder="Enter your password " name="password" value={values.password} onChange={handleChange}/>
+                      <input className="border p-2 rounded-lg outline-none" type="password" placeholder="Enter your password " name="password" value={values.password} onChange={handleChange}/>
                       {errors.password && <p className="error">{errors.password}</p>}
                   </div>
                   <div className="p-3">
@@ -57,9 +55,9 @@ import {GrLinkedin} from 'react-icons/gr'
                   <div className="p-3">
                       <p className="text-center">Or sign in using</p>
                       <div className="flex space-x-12 py-4 justify-center">
-                          <p>< FcGoogle/></p>
-                          <p><SiFacebook/></p>
-                          <p><GrLinkedin/></p>
+                          {/* <p><FcGoogle /></p>
+                          <p><SiFacebook /></p>
+                          <p><GrLinkedin /></p> */}
 
                       </div>
                       <p className="text-center">Already have an account? <strong className="text-blue-800">Login</strong></p>
